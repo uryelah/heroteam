c4a2b0df7f658dbaee1af95d97a69ebeefaa8d84
sarah <sarachamorro@gmail.com>
2019-09-11 15:26:55 -0300

    add redux boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +26,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Provider>
   );
 }
 
