56cdbb27db869c01599d889b2456714e70b16c17
sarah <sarachamorro@gmail.com>
2019-09-12 12:49:57 -0300

    add serve react-app build

--- webapp-backend/app.js
+++ webapp-backend/app.js
@@ -34,8 +34,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.use('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'build', 'índex.html'));
+  res.send("õk")
+})
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
