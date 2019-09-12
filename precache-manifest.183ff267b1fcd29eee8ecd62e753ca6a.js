1fdb6b5182ed71e8e8cac0c314621541abdb5767
sarah <sarachamorro@gmail.com>
2019-09-12 14:46:10 -0300

    test copyfile + build script

---
+++ precache-manifest.183ff267b1fcd29eee8ecd62e753ca6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4228735003255b5f73a1493e09f3a8",
+    "url": "/heroteam/index.html"
+  },
+  {
+    "revision": "77e64e0ee700f0da94b1",
+    "url": "/heroteam/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "45bd160770f2463f56fd",
+    "url": "/heroteam/static/js/2.6bffc8c4.chunk.js"
+  },
+  {
+    "revision": "77e64e0ee700f0da94b1",
+    "url": "/heroteam/static/js/main.33081d63.chunk.js"
+  },
+  {
+    "revision": "c7ae76a1d95bf4a233de",
+    "url": "/heroteam/static/js/runtime~main.042d1345.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/heroteam/static/media/logo.ee7cd8ed.svg"
+  }
+]);
