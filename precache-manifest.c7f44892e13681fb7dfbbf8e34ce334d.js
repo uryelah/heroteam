2a043cd0294da5a91fb781a53d485eb9e0098c19
sarah <sarachamorro@gmail.com>
2019-09-12 13:46:13 -0300

    Updates

---
+++ precache-manifest.c7f44892e13681fb7dfbbf8e34ce334d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad709203808b80b5e6316f28d0ad9193",
+    "url": "/heroteam/client/index.html"
+  },
+  {
+    "revision": "aa96530496bf352ddbb1",
+    "url": "/heroteam/client/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "45bd160770f2463f56fd",
+    "url": "/heroteam/client/static/js/2.6bffc8c4.chunk.js"
+  },
+  {
+    "revision": "aa96530496bf352ddbb1",
+    "url": "/heroteam/client/static/js/main.fb043560.chunk.js"
+  },
+  {
+    "revision": "b9e572227621a4d73391",
+    "url": "/heroteam/client/static/js/runtime~main.f6dd56b3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/heroteam/client/static/media/logo.ee7cd8ed.svg"
+  }
+]);
